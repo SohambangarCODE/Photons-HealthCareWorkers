61644df4dfce23c4322835d9115ac15f94618762
Soham Bangar <sohambangar188@gmail.com>
2026-02-21 23:32:58 +0530

    server.js fixed

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const dns = require("node:dns/promises");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
 const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
 const { Server } = require('socket.io');
 const connectDB = require('./config/db');
 
-dotenv.config();
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to Database
 connectDB();
 
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
