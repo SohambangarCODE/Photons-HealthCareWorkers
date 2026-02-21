fa78169dadb2751970e1f80f16d2e24448c3f9ad
Soham Bangar <sohambangar188@gmail.com>
2026-02-22 05:18:47 +0530

    deploymemnt issue fixed

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+const URL = import.meta.env.VITE_SOCKET_URL || 'https://photons-healthcareworkers.onrender.com';
 
 class SocketService {
   constructor() {
