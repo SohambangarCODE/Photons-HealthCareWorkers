b0bd92d47340bae324f8a3356445a94358b6cad6
Soham Bangar <sohambangar188@gmail.com>
2026-02-21 23:37:42 +0530

    paths fixed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const http = require('http');
 
 const app = express();
